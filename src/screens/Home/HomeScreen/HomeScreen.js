c42cef600339f5703ede26392a07bc1ac4ca09b7
lhmson <lehoangminhson123@gmail.com>
2021-04-29 14:56:39 +0700

    Edit app navigator

--- src/screens/Home/HomeScreen/HomeScreen.js
+++ src/screens/Home/HomeScreen/HomeScreen.js
@@ -3,14 +3,6 @@ import { View, Alert, Switch, ScrollView } from "react-native";
 import MyText from "../../../components/MyText/MyText";
 import COLOR from "../../../constants/colors";
 import styles from "./styles";
-import { Entypo } from "@expo/vector-icons";
-
-import MyButton from "../../../components/MyButton/MyButton";
-import MyCard from "../../../components/MyCard/MyCard";
-import MySwitch from "../../../components/MySwitch/MySwitch";
-import { DatePicker } from "native-base";
-import Loading from "../../../components/Loading/Loading";
-import MyTextInput from "../../../components/MyTextInput/MyTextInput";
 
 function HomeScreen() {
   return (
