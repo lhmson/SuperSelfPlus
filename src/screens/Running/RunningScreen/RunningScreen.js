1581ce70ecd16c8b1217d41b3e11f971861d3f5f
unknown <sanhlike1809@gmail.com>
2021-05-05 18:01:43 +0700

    add expo location

--- src/screens/Running/RunningScreen/RunningScreen.js
+++ src/screens/Running/RunningScreen/RunningScreen.js
@@ -1,32 +1,59 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Platform } from "react-native";
 import MyText from "../../../components/MyText/MyText";
 import MyCard from "../../../components/MyCard/MyCard";
 import COLOR from "../../../constants/colors";
 import styles from "./styles";
 import { Pedometer } from "expo-sensors";
+import * as Location from "expo-location";
 
 function RunningScreen() {
+  //Init Pedometer expo
   const [isPedometerAvailable, setIsPedometerAvailable] = React.useState(
     "checking"
   );
   const [pastStepCount, setPastStepCount] = React.useState(0);
   const [currentStepCount, setCurrentStepCount] = React.useState(0);
 
+  //Init Location expo
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
   React.useEffect(() => {
-    _subscribe();
+    _subscribePedometer();
+
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
   }, []);
+
+  let position = "Waiting..";
+  if (errorMsg) {
+    position = errorMsg;
+  } else if (location) {
+    position = location;
+  }
 
   return (
     <View style={styles.container}>
       <MyText b6>Running Screen</MyText>
-      <MyCard>
+      <MyCard style={{ flexDirection: "column" }}>
         <MyText>Pedometer : {currentStepCount} </MyText>
+        <MyText>
+          Location : {position.coords.latitude} {position.coords.longitude}{" "}
+        </MyText>
       </MyCard>
     </View>
   );
 
-  function _subscribe() {
+  function _subscribePedometer() {
     _subscription = Pedometer.watchStepCount((result) => {
       setCurrentStepCount(result.steps);
     });
