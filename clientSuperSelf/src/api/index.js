2a7218b7961a9efc8a90e02c986ba026a2c01bf3
lhmson <lehoangminhson123@gmail.com>
2021-05-21 15:23:27 +0700

    Hide back url

--- clientSuperSelf/src/api/index.js
+++ clientSuperSelf/src/api/index.js
@@ -1,14 +1,20 @@
 import axios from "axios";
-
 import { backend_url } from "react-native-dotenv";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const API = axios.create({ baseURL: "http://192.168.1.6:5000" });
+const API = axios.create({ baseURL: backend_url });
 
-// API.interceptors.request.use((req) => {
-//   if (localStorage.getItem("user")) {
-//     req.headers.Authorization = `Bearer ${
-//       JSON.parse(localStorage.getItem("user")).token
-//     }`;
+// API.interceptors.request.use(async (req) => {
+//   let data = null;
+//   try {
+//     data = JSON.parse(await AsyncStorage.getItem("token"));
+//     // alert(JSON.stringify(data));
+//   } catch (error) {
+//     alert("Cannot not get storage");
+//     console.log("Error in storage", error);
+//   }
+//   if (token) {
+//     req.headers.Authorization = `Bearer ${data.token}`;
 //   }
 
 //   return req;
