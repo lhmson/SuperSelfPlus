8fb0adb7fc573a9e01400f2142576824f55468a1
unknown <sanhlike1809@gmail.com>
2021-05-31 15:31:49 +0700

    fix bugs null coordinate

--- clientSuperSelf/src/screens/Running/MapRunningScreen/index.js
+++ clientSuperSelf/src/screens/Running/MapRunningScreen/index.js
@@ -401,12 +401,14 @@ const MapRunningScreen = ({ navigation }) => {
           pushCoordinateIntoRoad(locationChangedResult.nativeEvent.coordinate);
         }}
       >
+      {
+        roadRunCoordinate[0]?
         <Marker coordinate={roadRunCoordinate[0] ?? null}>
           <Image
             source={ICON.startRun}
             style={{ height: 60, width: 60, resizeMode: "contain" }}
           />
-        </Marker>
+        </Marker> : null}
         <Polyline
           coordinates={roadRunCoordinate}
           strokeColor={COLOR.green}
