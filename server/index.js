48e786f94d775076abdefe4e6f0fc50bb8d4c59c
lhmson <lehoangminhson123@gmail.com>
2021-07-03 08:34:16 +0700

    Add temp message

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import path, { dirname } from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import { fileURLToPath } from "url";
-
+import jwt from "jsonwebtoken";
 import cloudinary from "cloudinary";
 const cloudinaryv2 = cloudinary.v2;
 
@@ -23,6 +23,7 @@ import eventRouter from "./routes/event.js";
 import uploadRouter from "./routes/upload.js";
 import postRouter from "./routes/post.js";
 import tinderRouter from "./routes/tinder.js";
+// import messageRouter from "./routes/message.js";
 
 dotenv.config();
 
@@ -64,6 +65,7 @@ app.use("/run", runRouter);
 app.use("/upload", uploadRouter);
 app.use("/tinder", tinderRouter);
 app.use("/post", postRouter);
+// app.use("/message", messageRouter);
 
 const PORT = process.env.PORT || 5000;
 
@@ -81,3 +83,28 @@ mongoose
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set("useFindAndModify", false);
+
+// io.on("connection", async (socket) => {
+//   console.log("New connect");
+//   // join
+//   socket.on("join-client", async ({ token, userIds }) => {
+//     const verified = jwt.verify(token, process.env.JWT_ACCESS_KEY);
+//     const rooms = [];
+//     for (let userId of userIds) {
+//       const key = [verified._id.toString(), userId.toString()].sort((a, b) =>
+//         a < b ? -1 : 1
+//       );
+//       const roomId = `${key[0]}-${key[1]}`;
+//       rooms.push(roomId);
+//     }
+//     socket.join(rooms);
+//     socket.emit("join-server", {
+//      message:"Joined successfully"
+//     });
+//   });
+
+//   // disconnect
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//   });
+// });
